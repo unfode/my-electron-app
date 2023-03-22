0a4d0d3e05686d79e4e4dd5b855b1a312761336d
unfode <forrest.futao.wei@gmail.com>
2023-03-22 13:03:33 -0400

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,9 @@
+const info = document.getElementById('info')
+info.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+  const response = await window.versions.ping()
+  console.log('response') // prints out 'pong'
+}
+
+func()
